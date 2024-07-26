ca35dd4e53893f849d12790c3aa8cf9964fa9e1c
FelipeFreitas96 <felipe.samidare@hotmail.com>
2024-07-26 15:54:45 -0300

    chore: adiciona correção de problema em funcionalidade de contagem em array

--- services/word_service.py
+++ services/word_service.py
@@ -5,7 +5,7 @@ class WordService:
         return sum([len(w) for w in word if unidecode(w).lower() in 'aeiou'])
     
     def getVowalsFromArray(self, words):
-        return {word: self.getVowalsCount(word) for word in words}
+        return {word: self.getVowalsCount(word) for word in words if len(word) > 0}
 
     def sortWordsFromArray(self, words, order):
         return words.sort(reverse=order == 'asc') or words
