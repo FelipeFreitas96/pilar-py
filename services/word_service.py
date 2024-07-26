a792bd10c51de5035b2c8aa046f6ee0abe9c2de5
FelipeFreitas96 <felipe.samidare@hotmail.com>
2024-07-26 15:44:14 -0300

    fix: corrige problema de contagem de vogais com acentos

--- services/word_service.py
+++ services/word_service.py
@@ -1,6 +1,8 @@
+from unidecode import unidecode
+
 class WordService:
     def getVowalsCount(self, word):
-        return sum([len(w) for w in word if w.lower() in 'aeiou'])
+        return sum([len(w) for w in word if unidecode(w).lower() in 'aeiou'])
     
     def getVowalsFromArray(self, words):
         return {word: self.getVowalsCount(word) for word in words}
