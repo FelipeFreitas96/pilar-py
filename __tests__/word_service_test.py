cf575f8c8f77d9b2d4f2cfc8c2499a9620abc71a
FelipeFreitas96 <felipe.samidare@hotmail.com>
2024-07-26 15:54:34 -0300

    test: adiciona testes da contagem de vogais por array

--- __tests__/word_service_test.py
+++ __tests__/word_service_test.py
@@ -2,7 +2,7 @@ from services.word_service import WordService
 
 service = WordService()
 
-def test_getword():
+def test_get_vowals_count():
     assert service.getVowalsCount('hello') == 2
     assert service.getVowalsCount('world') == 1
     assert service.getVowalsCount('aeiou') == 5
@@ -12,4 +12,11 @@ def test_getword():
     assert service.getVowalsCount('aeiouaeiouaeiouaeiouaeiou') == 25
     assert service.getVowalsCount('"!@#$%¨&*()_+{}:?></*-+,0°¹²³£¢¬~´`₢') == 0
     assert service.getVowalsCount('àáèéìíòóùú') == 10
+    
+def test_get_vowals_from_array():
+    assert service.getVowalsFromArray(['hello', 'world', 'aeiou', '']) == { 'hello': 2, 'world': 1, 'aeiou': 5 }
+    assert service.getVowalsFromArray(['aaaaaaaaa', 'bbbbbbbb', 'aeiouaeiouaeiouaeiouaeiou']) == { 'aaaaaaaaa': 9, 'bbbbbbbb': 0, 'aeiouaeiouaeiouaeiouaeiou': 25 }
+    assert service.getVowalsFromArray(['"!@#$%¨&*()_+{}:?></*-+,0°¹²³£¢¬~´`₢', 'àáèéìíòóùú']) == { '"!@#$%¨&*()_+{}:?></*-+,0°¹²³£¢¬~´`₢': 0, 'àáèéìíòóùú': 10 }
+    assert service.getVowalsFromArray(['']) == {}
+    assert service.getVowalsFromArray([]) == {}
     
